87d559cad26439d41276942d1744e9d8d7abf32d
João Louzada <joaolouzada2017@gmail.com>
2025-07-01 14:51:24 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">{todo.category}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
